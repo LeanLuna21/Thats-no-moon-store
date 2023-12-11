c16f9412eae1006e087f1d08ab34a0b0ab59e1ed
LeandroGastonLuna <lea21luna@gmail.com>
2023-12-11 09:22:35 -0300

    login_required creado y templates modificados

--- SitioMain/SitioMain/views.py
+++ SitioMain/SitioMain/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # acá voy a agregar las fx para modificar el template
 # crear fx que rendericen las htm
+
+@login_required
 def principal(request):
     return render(request,'index.html')

957be71f465a2810400384ee48db8a7692af61a9
Leandro <109328793+LeanLuna21@users.noreply.github.com>
2023-12-13 21:39:00 -0300

    Delete ThatsNoMoonStore directory

--- ThatsNoMoonStore/personas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from personas.models import PerfilUsuario,Cliente,Transaccion
-# Register your models here.
-admin.site.register(PerfilUsuario)
-admin.site.register(Cliente)
-admin.site.register(Transaccion)
--- ThatsNoMoonStore/stock/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# registramos los modelos (impacta en la bbdd)
-from stock.models import Sable,Componente,Crystal
-
-admin.site.register(Sable)
-admin.site.register(Componente)
-admin.site.register(Crystal)
